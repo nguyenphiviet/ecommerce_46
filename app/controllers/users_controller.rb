b1e09486d3f6d3aad241ab47c537d8e7f8573a6d
Nguyen Phi Viet <nguyen.phi.viet@framgia.com>
2018-05-15 18:46:44 +0700

    [admin] show list user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:show, :edit, :update, :destroy]
+  before_action :logged_in_user, only: [:show, :edit, :update]
 
   def show; end
 
